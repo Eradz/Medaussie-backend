35b4c83f556740cd7bf5e9a76757120b94db6ffc
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-28 23:18:19 +0100

    changed post

--- src/controllers/Post/GetSinglePost.ts
+++ src/controllers/Post/GetSinglePost.ts
@@ -4,8 +4,13 @@ import AsyncHandler from "express-async-handler"
 import { Post } from "../../models"
 
 export const getSinglePostController = AsyncHandler(async(req: Request, res: Response) =>{
-    const {id} = req.params
-    const user = await Post.findById(id).select("-_v")
+    const {slug} = req.params
+    const {type} = req.query
+    if(!type){
+        AppResponse.error(res, "Please provide a type of post to be rendered")
+        return
+    }
+    const user = await Post.findOne({slug, type}).select("-_v")
     if(!user){
         AppResponse.error(res, "Post not found")
         return
